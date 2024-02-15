7cd80ef99814e546dbbffd81bc5c1eff31a35105
Amaro Suárez <amaro.suarez@iesnervion.es>
2024-02-15 11:22:23 +0100

    Ejercicio 8

---
+++ src/boletin05/Ejercicio08.java
@@ -0,0 +1,50 @@
+package boletin05;
+
+import java.util.Scanner;
+
+public class Ejercicio08 {
+
+	public static void main(String[] args) {
+		
+		// Variable donde se almacanerá la cadena
+		String cadena = "";
+		
+		// Creamos el Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		// Le pedimos la cadena al usuario y la guardamos
+		System.out.println("Introduzca una cadena sin tildes");
+		cadena = sc.nextLine();
+		
+		// Llamamos a la función
+		System.out.println(
+				"La palabra " + (esPalindroma(cadena) ? "es" : "no es") + " palíndroma"
+		);
+		
+		// Cerramos el Scanner
+		sc.close();
+	}
+	
+	/**
+	 * Función que recibe una cadena y devuelve si es palindroma o no
+	 * @param cadena Cadena que se comprobará si es palíndroma
+	 * @return Devuelve si la cadena es palindroma
+	 */
+	static boolean esPalindroma (String cadena) {
+		// Variable donde se almacanerá si es palindroma
+		boolean palindroma = false;
+		
+		// Variable donde se almacenará la cadena inversa
+		String cadenaInvertida = "";
+		
+		// Voy a reutilizar la función del ejercicio 5
+		cadenaInvertida = Ejercicio05.cadenaInversa(cadena);
+		
+		// Comprobamos si son iguales
+		if (cadenaInvertida.equalsIgnoreCase(cadena)) palindroma = true;
+		
+		// Devolvemos el resultado
+		return palindroma;
+	}
+	
+}
