b5b9b4e7173943b8103bd515d15a1d750b51cd8c
Amaro Suárez <amaro.suarez@iesnervion.es>
2024-02-16 14:18:38 +0100

    Ejercicio 13 hecho

---
+++ src/boletin05/Ejercicio13.java
@@ -0,0 +1,58 @@
+package boletin05;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ejercicio13 {
+	
+	public static void main(String[] args) {
+		// Variable donde se almacenará la primera cadena
+		String cadena1 = "";
+		
+		// Variable donde se almacenará la segunda cadena
+		String cadena2 = "";
+		
+		// Creamos el Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		// Le pedimos la primera frase
+		System.out.println("Introduzca una frase o palabra");
+		cadena1 = sc.nextLine().toLowerCase();
+		
+		// Le pedimos la primera frase
+		System.out.println("Introduzca otra");
+		cadena2 = sc.nextLine().toLowerCase();
+			
+		// Llamamos a la función y mostramos el mensaje por pantalla
+		System.out.println("La frase o palabra " + (esAnagrama(cadena1, cadena2) ? "es" : "no es") + " anagrama");
+		
+		// Cerramos el Scanner
+		sc.close();
+	}
+	
+	static boolean esAnagrama (String cadena1, String cadena2) {
+		// Variable que almacenará si es anagrama
+		boolean anagrama = false;
+		
+		// Array de char donde se almacenará la cadena1
+		char[] tabla1;
+		
+		// Array de char donde se almacenará la cadena2
+		char[] tabla2;
+		
+		// Convertimos las cadenas al array y las ordenamos
+		tabla1 = cadena1.toCharArray();
+		tabla2 = cadena2.toCharArray();
+		
+		// Ordenamos las tablas
+		Arrays.sort(tabla1);
+		Arrays.sort(tabla2);
+		
+		// Comparamos si una vez ordenados son iguales
+		if (Arrays.equals(tabla1, tabla2)) anagrama = true;
+		
+		// Devolvemos el boolean
+		return anagrama;
+	}
+
+}
